5a0da088a4189a7cf8c8c0ff342ac02fa556f46e
Tobi Sim <dasimmet@simmethome.fritz.box>
2016-06-18 13:51:03 +0200

    added isKeyFor to find keys by email

--- qml/pyLib/crypt.py
+++ qml/pyLib/crypt.py
@@ -1,3 +1,15 @@
-import gnupg
+import gnupg,re
 
 gpg = gnupg.GPG(options=["--trust-model","always"])
+
+_keypattern = re.compile(r".*<(.*)>.*")
+def isKeyFor(uids,email):
+    '''
+Takes a List of key uids (from GPG.list_keys()) and checks if they contain "email"
+    '''
+    acc = False
+    for uid in uids:
+        mat = _keypattern.match(uid)
+        if mat and mat.group(1) == email:
+            acc = True
+    return acc
